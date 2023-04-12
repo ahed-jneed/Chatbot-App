4aa4d99bccb6a7fa4ffe1055d6b62d5f125b01e8
Ahed Jneed <77403824+ahed-jneed@users.noreply.github.com>
2023-04-12 23:32:59 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from streamlit.server import Server
 from langchain.chains import ConversationChain
 from langchain.chains.conversation.memory import ConversationEntityMemory
 from langchain.chains.conversation.prompt import ENTITY_MEMORY_CONVERSATION_TEMPLATE
